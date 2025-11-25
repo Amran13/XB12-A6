c40d00a92c2d319324ae1359429521c73b18d739
amranhossain <amranhossainchy@gmail.com>
2025-11-25 13:15:42 +0600

    category added dynamically

--- script/app.js
+++ script/app.js
@@ -1,1 +1,25 @@
 console.log('hello wrold')
+
+
+const loadCategories = () => {
+    fetch(`https://openapi.programming-hero.com/api/categories`)
+    .then(res => res.json())
+    .then(data => displayCategories(data.categories))
+}
+
+
+
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('catogory-container')
+    categoryContainer.innerHTML = ""
+    for(category of categories){
+        const btnDiv = document.createElement("div")
+        btnDiv.innerHTML = `
+        <button class="btn btn-wide hover:bg-green-600 hover:text-white bg-green-50 border-none"> ${category.category_name} </button>
+        `
+        categoryContainer.append(btnDiv)
+    }
+}
+
+
+loadCategories()
